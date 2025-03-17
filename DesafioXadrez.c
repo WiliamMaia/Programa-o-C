71d21eb990239811c4c6c0a6610d0ee44630687f
WiliamMaia <wiliam.maia.silva@gmail.com>
2025-03-17 12:51:14 +0000

    # trabalhei na correção do trabalho com os apontamentos do professor

--- DesafioXadrez.c
+++ DesafioXadrez.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
 void movetorre(int movimento){  // Aqui declaro a função para movimento da Torre
-    if (movimento > 0){
-        printf("Direita ! \n");
-        movetorre(movimento - 1);
-    }
+    for (movimento; movimento > 0; movimento --)
+        printf("Direita \n");
 }
-void movebispo(int movimento){ // Aqui declaro a função para movimento do Bispo
-    while (movimento > 0){
-        printf("Direita e Cima \n");
-        movimento --;
-    }
-}
-void moverainha(int movimento){  // Aqui declaro a função para movimento da Rainha
-    do { printf("Esquerda ! \n");
-        movimento --;
-    } while (movimento > 0);
+void movebispo(int movimento){ //  Aqui declaro a função para movimento do Bispo
+    
 }
 
 int main() {
 
-    int escolhapeca,movimento = 1; // Aqui declaro as variáveis
+    int escolhapeca; // Aqui declaro a variável
 
     printf("Vamos movimentar as peças de Xadrez ! \n");
     printf("Primeiro escolha qual peça irá se movimentar: \n 1. Torre \n 2. Bispo \n 3. Rainha \n 4. Cavalo \n"); // Aqui solicito a escolha da peça
@@ -29,21 +19,19 @@ int main() {
     switch (escolhapeca){ // switch para escolha de peça
         case 1: // Escolha da Torre
         movetorre(5);
-         break;  // Fim Torre
+        break;  // Fim Torre
         case 2: // Escolha Bispo
-        movebispo(5);
+
         break;  // Fim Bispo
         case 3: // Escolha Rainha
-        moverainha(8);
+
         break;  // Fim Rainha
         case 4: // Escolha Cavalo
-        while (movimento --){
-            for (int avanco_cavalo = 2;avanco_cavalo > 0; avanco_cavalo --){
-                printf("Cima \n");
-            }
-                printf("Direita \n");
-        }
-        break;  // Fim Cavalo
+        for (int avanco_cavalo = 2;avanco_cavalo > 0; avanco_cavalo --){
+            printf("Cima \n");
+        } for (int movimento = 1; movimento > 0; movimento --){
+            printf("Direita \n");
+        } break;  // Fim Cavalo
         default:
         printf("Escolha Inválida !\n");
         break; // Fim escolha de peça
